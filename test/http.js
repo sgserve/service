bd9b4e369f072b2c235d324d36c59aebad31e8b9
cupdir <cupdir@gmail.com>
2018-04-09 08:53:05 +0800

    修复三国杀位置资源名称

--- test/http.js
+++ test/http.js
@@ -1,1 +1,14 @@
 const sgsc = require('./../lib').createServer(80)
+
+const hostile = require('hostile')
+
+process.on('SIGINT', ()=> {
+    hostile.remove('127.0.0.1', 'web.sanguosha.com', (err)=> {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log('set /etc/hosts successfully!')
+        process.exit();
+      }
+    })
+});
